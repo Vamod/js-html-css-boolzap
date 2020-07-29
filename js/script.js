d82acb19e59554828474c5c96f44c27365b8ddac
Valerio <valerio.modesti@gmail.com>
2020-07-29 15:36:48 +0200

    tentativo js n1

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-
+// alla pressione di enter invio il messaggio
     $('#chat-type').keydown(myMessage);
 
     function myMessage(){
         if (event.which == 13 || event.keyCode == 13) {
-            // prendo il contenuto
+            // prendo il contenuto dell'input
             var valore = $('#chat-type').val();
             // per inserire faccio il clone
             var el = $('.my-chat-container').clone();
             // ho personalizzato il clone
-            el.find('.chat-time').append(time())
+            el.find('.chat-time').append(time());
             el.find('.my-chat-text').append(valore);
-
+            //lo rendo visibile aggiungendo display flex
             el.addClass('flex-right');
 
             // lo aggiungo al document
 
             $('.chat-area-margin').append(el);
         }
-
+//funzione per stabile l'ora
         function time(){
             var d = new Date();
             var hour = d.getHours();
@@ -27,6 +27,16 @@ $(document).ready(function(){
             return time;
 
         }
+    }
+
+    setTimeout(answer, 1000);
+
+    function answer(){
+        var el = $('.friend-chat-container').clone();
+        el.find('.friend-chat-text').append('ok');
+        el.find('.chat-time').append(time());
+        $('.friend-chat-container').addClass('flex-left');
+        $('.chat-area-margin').append(el);
     }
 
 });
