68467787f5a87ed32fac71e14b39172b96c7a53c
Valerio <valerio.modesti@gmail.com>
2020-07-30 16:06:38 +0200

    bozza completa

--- js/script.js
+++ js/script.js
@@ -7,36 +7,43 @@ $(document).ready(function(){
 
     // al click su send invia il messaggio
     $('#send').click(function(){
+        //prendo valore input
         var valore = $('#chat-type').val();
+        //controllo che il messaggio non sia vuoto
+        if (valore != '') {
         // per inserire faccio il clone
         var el = $('.template-mine .my-chat-container').clone();
         // ho personalizzato il clone
         el.find('.chat-time').append(time());
         // aggiungo il valore alla chat
         el.find('.my-chat-text').append(valore);
-        // lo aggiungo al document
+        // lo aggiungo al document .active
         $('.chat-area-margin.active').append(el);
 
         // richiamo funzione automatica di risposta con un secondo di ritardo
         setTimeout(answer, 1000);
+        }
     });
 
     //per selezionare la chat
     $('.friend').click(function(){
         //prendo posizione dei contatti sulla sx
         var posizione = $(this).index();
-        console.log(posizione);
-        //rimuovo classe active
+        //rimuovo classe active dalla chat
         $('.chat-area .chat-area-margin').removeClass('active');
         //aggiungo la classe active alla chat di indice selezionato dai contatti
         $('.chat-area .chat-area-margin').eq(posizione).addClass('active');
+        // tolgo la classe .active
         $('.friend').removeClass('active');
         $('.friend').eq(posizione).addClass('active');
 
 
     });
 
+//*************funzioni**************
+
     // funzione myMessage per inviare msg
+
     function myMessage(){
         if (event.which == 13 || event.keyCode == 13) {
             // prendo il contenuto dell'input
@@ -77,10 +84,24 @@ $(document).ready(function(){
                 // per pulire il campo di input
                 $('#chat-type').val('');
                 var el = $('.template-answer .friend-chat-container').clone();
-                el.find('.friend-chat-text').append('ok');
+                // creo un array di frasi random
+                var frasiRandom = [
+                    'Ciao!',
+                    'Come stai?',
+                    'Ci possiamo sentire dopo?',
+                    'Oggi è una bella giornata!',
+                    'Sono in riunione'
+                ];
+                var indice = numeriRandom(0, frasiRandom.length - 1);
+                el.find('.friend-chat-text').append(frasiRandom[indice]);
                 el.find('.chat-time').append(time());
                 $('.chat-area-margin.active').append(el);
 
+            }
+
+            //funzione random 
+            function numeriRandom(min, max){
+                return Math.floor(Math.random()*(max - min + 1) + min);
             }
 
 
