ee1b32eec3bbbacc818af696738c5ef31c1954a1
Valerio <valerio.modesti@gmail.com>
2020-07-30 08:58:59 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -2,32 +2,18 @@ $(document).ready(function(){
 // alla pressione di enter invio il messaggio
     $('#chat-type').keydown(myMessage);
 
-    function myMessage(){
-        if (event.which == 13 || event.keyCode == 13) {
-            // prendo il contenuto dell'input
-            var valore = $('#chat-type').val();
-            // per inserire faccio il clone
-            var el = $('.template-mine .my-chat-container').clone();
-            // ho personalizzato il clone
-            el.find('.chat-time').append(time());
-            el.find('.my-chat-text').append(valore);
 
-            // lo aggiungo al document
-            $('.chat-area-margin').append(el);
 
-            // richiamo funzione automatica di risposta con un secondo di ritardo
-            setTimeout(answer, 1000);
-        }
-    }
 
+    // al click su senda invia il messaggio
     $('#send').click(function(){
         var valore = $('#chat-type').val();
         // per inserire faccio il clone
         var el = $('.template-mine .my-chat-container').clone();
         // ho personalizzato il clone
         el.find('.chat-time').append(time());
+        // aggiungo il valore alla chat
         el.find('.my-chat-text').append(valore);
-
         // lo aggiungo al document
         $('.chat-area-margin').append(el);
 
@@ -36,6 +22,24 @@ $(document).ready(function(){
     });
 
 
+    // funzione myMessage
+    function myMessage(){
+        if (event.which == 13 || event.keyCode == 13) {
+            // prendo il contenuto dell'input
+            var valore = $('#chat-type').val();
+            // per inserire faccio il clone
+            var el = $('.template-mine .my-chat-container').clone();
+            // ho personalizzato il clone
+            el.find('.chat-time').append(time());
+            // aggiungo il valore alla chat
+            el.find('.my-chat-text').append(valore);
+            // lo aggiungo al document
+            $('.chat-area-margin').append(el);
+
+            // richiamo funzione automatica di risposta con un secondo di ritardo
+            setTimeout(answer, 1000);
+        }
+    }
             //funzione per stabile l'ora
             function time(){
                 var d = new Date();
@@ -47,6 +51,8 @@ $(document).ready(function(){
 
             // funzione di risposta automatica 'ok'
             function answer(){
+                // per pulire il campo di input
+                $('#chat-type').val('');
                 var el = $('.template-answer .friend-chat-container').clone();
                 el.find('.friend-chat-text').append('ok');
                 el.find('.chat-time').append(time());
